3f838f168c8f7d47ea0511701bc439b074e0131d
diegolopezgrisanti <diegolopezgrisanti@gmail.com>
2025-01-30 12:16:37 +0100

    refactor PriceControllerIntegrationTest

--- src/test/java/com/inditex/price/service/infrastructure/entrypoint/rest/PriceControllerIntegrationTest.java
+++ src/test/java/com/inditex/price/service/infrastructure/entrypoint/rest/PriceControllerIntegrationTest.java
@@ -1,116 +1,71 @@
 package com.inditex.price.service.infrastructure.entrypoint.rest;
 
-import com.inditex.price.service.domain.models.Price;
-import com.inditex.price.service.domain.usecases.FindPricesUseCase;
+import com.inditex.price.service.domain.exception.InvalidDateFormatException;
 import com.inditex.price.service.infrastructure.entrypoint.rest.response.PriceResponseDTO;
-import com.inditex.price.service.infrastructure.mappers.PriceMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Currency;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class PriceControllerIntegrationTest {
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private FindPricesUseCase findPricesUseCase;
-
-    @MockBean
-    private PriceMapper priceMapper;
-
+    private PriceController priceController;
 
     @Test
-    void shouldReturnPriceWhenValidInput() throws Exception {
+    void shouldReturnPriceWhenValidInput() {
+        // GIVEN
         Long productId = 35455L;
         Long brandId = 1L;
         String dateTime = "2020-06-14T10:00:00";
 
-        Price mockPrice = new Price(
-                1L,
-                brandId,
-                LocalDateTime.of(2020, 6, 14, 0, 0, 0),
-                LocalDateTime.of(2020, 12, 31, 23, 59, 59),
-                1,
-                productId,
-                0,
-                BigDecimal.valueOf(35.50),
-                Currency.getInstance("EUR")
-        );
+        PriceResponseDTO expectedResponse = new PriceResponseDTO();
+        expectedResponse.setProductId(35455L);
+        expectedResponse.setBrandId(1L);
+        expectedResponse.setPriceList(1);
+        expectedResponse.setStartDate(LocalDateTime.of(2020, 6, 14, 0, 0, 0));
+        expectedResponse.setEndDate(LocalDateTime.of(2020, 12, 31, 23, 59, 59));
+        expectedResponse.setFinalPrice(BigDecimal.valueOf(35.50));
+        expectedResponse.setCurrency(Currency.getInstance("EUR"));
 
-        PriceResponseDTO mockPriceResponseDTO = new PriceResponseDTO();
-        mockPriceResponseDTO.setProductId(35455L);
-        mockPriceResponseDTO.setBrandId(1L);
-        mockPriceResponseDTO.setPriceList(1);
-        mockPriceResponseDTO.setStartDate(LocalDateTime.of(2020, 6, 14, 0, 0, 0));
-        mockPriceResponseDTO.setEndDate(LocalDateTime.of(2020, 12, 31, 23, 59, 59));
-        mockPriceResponseDTO.setFinalPrice(BigDecimal.valueOf(35.50));
-        mockPriceResponseDTO.setCurrency(Currency.getInstance("EUR"));
+        // WHEN
+        ResponseEntity<PriceResponseDTO> responseEntity = priceController.getPrices(productId, brandId, dateTime);
+        PriceResponseDTO actualResponse = responseEntity.getBody();
 
-        when(findPricesUseCase.execute(productId, brandId, LocalDateTime.parse(dateTime)))
-                .thenReturn(mockPrice);
-        when(priceMapper.toResponseDTO(mockPrice)).thenReturn(mockPriceResponseDTO);
+        // THEN
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(actualResponse).isNotNull();
 
-        mockMvc.perform(get("/prices")
-                        .param("productId", productId.toString())
-                        .param("brandId", brandId.toString())
-                        .param("dateTime", dateTime))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.productId").value(35455))
-                .andExpect(jsonPath("$.brandId").value(1))
-                .andExpect(jsonPath("$.priceList").value(1))
-                .andExpect(jsonPath("$.startDate").value("2020-06-14T00:00:00"))
-                .andExpect(jsonPath("$.endDate").value("2020-12-31T23:59:59"))
-                .andExpect(jsonPath("$.finalPrice").value(35.50))
-                .andExpect(jsonPath("$.currency").value("EUR"));
+        assertThat(actualResponse)
+                .usingRecursiveComparison()
+                .ignoringFields("finalPrice")
+                .isEqualTo(expectedResponse);
+        assertThat(actualResponse.getFinalPrice().stripTrailingZeros())
+                .isEqualTo(expectedResponse.getFinalPrice().stripTrailingZeros());
     }
 
     @Test
-    void shouldReturnNotFoundWhenPriceNotFound() throws Exception {
-        Long productId = 35455L;
-        Long brandId = 1L;
-        String dateTime = "2020-06-14T10:00:00";
-
-        when(findPricesUseCase.execute(productId, brandId, LocalDateTime.parse(dateTime)))
-                .thenReturn(null);
-
-        mockMvc.perform(get("/prices")
-                        .param("productId", productId.toString())
-                        .param("brandId", brandId.toString())
-                        .param("dateTime", dateTime))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.message").value(
-                        String.format("Price not found for the given productId: %d and brandId: %d", productId, brandId)
-                ));
-    }
-
-    @Test
-    void shouldReturnBadRequestWhenDateFormatIsInvalid() throws Exception {
+    void shouldReturnBadRequestWhenDateFormatIsInvalid() {
+        // GIVEN
         long productId = 35455L;
         long brandId = 1L;
         String invalidDateTime = "invalid-date-format";
 
-        mockMvc.perform(get("/prices")
-                        .param("productId", Long.toString(productId))
-                        .param("brandId", Long.toString(brandId))
-                        .param("dateTime", invalidDateTime))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value(
-                        "Invalid date format: " + invalidDateTime
-                ));
+        // WHEN & THEN
+        InvalidDateFormatException exception = assertThrows(
+                InvalidDateFormatException.class,
+                () -> priceController.getPrices(productId, brandId, invalidDateTime)
+        );
+
+        assertEquals("Invalid date format: " + invalidDateTime, exception.getMessage());
     }
 }
